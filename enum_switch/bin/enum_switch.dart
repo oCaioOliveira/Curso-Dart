70333282e99bfca723d697240413f1e3815e2bbf
oCaioOliveira <caiocesar31951910@hotmail.com>
2023-08-12 16:39:54 -0300

    Add Set

--- enum_switch/bin/enum_switch.dart
+++ enum_switch/bin/enum_switch.dart
@@ -3,6 +3,45 @@ void main(List<String> arguments) {
 
   WeekDay day = WeekDay.sunday;
 
+  selectDay(day);
+
+  List<String> destinations = [];
+  String destination = "São Paulo";
+  destinations.add(destination);
+  destinations.add(destination);
+  print(destinations);
+
+  Set<String> visitedRegisters = <String>{};
+  visitedRegisters = registerDestinatiion(destination, visitedRegisters);
+  visitedRegisters = registerDestinatiion("Rio de Janeiro", visitedRegisters);
+  visitedRegisters = registerDestinatiion("Recife", visitedRegisters);
+  visitedRegisters = registerDestinatiion("Recife", visitedRegisters);
+  visitedRegisters = registerDestinatiion("Rio de Janeiro", visitedRegisters);
+  print(visitedRegisters);
+
+  print(visitedRegisters.contains("Recife"));
+  print(visitedRegisters.contains("Natal"));
+
+  print("List of Destionations:");
+  visitedRegisters.forEach((String destination) {
+    print(destination);
+  });
+  // visitedRegisters.forEach((String destination) => print(destination)); ALTERNATIVE WAY
+
+  Map<String, double> registerPrices = {};
+
+  registerPrices["São Paulo"] = 100.00;
+  registerPrices["Recife"] = 150.00;
+
+  print(registerPrices);
+  print(registerPrices["Recife"]);
+
+  registerPrices.remove("Recife");
+
+  print(registerPrices);
+}
+
+selectDay(Enum day) {
   switch (day) {
     case WeekDay.sunday:
       print('Today is Sunday');
@@ -19,6 +58,12 @@ void main(List<String> arguments) {
     default:
       print('Today is not a day of the week');
   }
+}
+
+Set<String> registerDestinatiion(String destination, Set<String> dataBase) {
+  dataBase.add(destination);
+
+  return dataBase;
 }
 
 enum WeekDay { sunday, monday, tuesday, wednesday, thursday, friday, saturday }
